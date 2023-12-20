97e2b07548b3d253c756a9205b34ab5701460dd2
Mohamadhalabi <mhmd_Halabi99@hotmail.com>
2023-12-20 14:46:36 +0300

    Seo has bene added to static pages

--- pages/terms.vue
+++ pages/terms.vue
@@ -90,7 +90,130 @@
 
 <script>
 export default {
-  name: "terms"
+  name: "terms",
+  head() {
+    return {
+      title: "Techno Lock Keys Trading | Automotive Locksmith Services",
+      link: [
+        {
+          rel: 'canonical',
+          href: 'https://www.tlkeys.com/terms',
+        },
+      ],
+      meta: [
+        {
+          "http-equiv": "content-language",
+          content: this.$i18n.locale,
+        },
+        {
+          name: 'description',
+          content: "Explore our concise terms for locksmith services. Trust Techno Lock Keys for security with integrity and professionalism."
+        },
+        {
+          name: "og:type",
+          content: "website",
+        },
+        {
+          name: "og:site_name",
+          content: "Techno Lock Keys",
+        },
+        {
+          name: "og:title",
+          content: "Techno Lock Keys Trading | Automotive Locksmith Services",
+        },
+        {
+          name: "og:description",
+          content: "Techno Lock Keys provides a wide range of auto keys, remotes, diagnostics, cutting machines, programming devices, Fobs, transponder keys, and emulators"
+        },
+        {
+          name: "og:url",
+          content: "https://www.tlkeys.com",
+        },
+        {
+          name: "og:image",
+          content: "https://dev-srv.tlkeys.com/storage/images/seo/technolock_logo.jpg",
+        },
+        {
+          name: "og:image:alt",
+          content: this.$settings.seo.meta_image.l.alt,
+        },
+        {
+          name: "og:image:height",
+          content: "627",
+        },
+        {
+          name: "og:image:width",
+          content: "1200",
+        },
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          name: "twitter:site",
+          content: `${this.$settings.social_media.twitter}`,
+        },
+        {
+          name: "twitter:title",
+          content: 'Techno Lock Keys Trading | Automotive Locksmith Services',
+        },
+        {
+          name: "twitter:description",
+          content: "Techno Lock Keys provides a wide range of auto keys, remotes, diagnostics, cutting machines, programming devices, Fobs, transponder keys, and emulators"
+        },
+        {
+          rel: "shortcut icon",
+          href: "https://dev-srv.tlkeys.com/storage/images/seo/favicon-tlkeys.png",
+        },
+      ],
+      script: [
+        { type: 'application/ld+json', json: {
+            "@context": "http://schema.org/",
+            "@type": "Organization",
+            "name": "Techno Lock Keys Trading",
+            "url": "https://www.tlkeys.com/",
+            "image": "https://www.tlkeys.com/tlk-logo.png",
+            "description": "Techno Lock Keys provides a wide range of auto keys, remotes, diagnostics, cutting machines, programming devices, Fobs, transponder keys, and emulators.",
+            "address": {
+              "@type": "PostalAddress",
+              "streetAddress": "Industrial No. 5, behind Maliah Road., shop No. 8",
+              "addressCountry": "AE"
+            },
+            "telephone": "+971 6 542 1313",
+            "openingHoursSpecification": [
+              {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                  "Monday",
+                  "Tuesday",
+                  "Wednesday",
+                  "Thursday",
+                  "Saturday",
+                  "Sunday"
+                ],
+                "opens": "10:00",
+                "closes": "18:00"
+              }
+            ],
+            "priceRange": "$$",
+            "paymentAccepted": "Cash, Credit Card, Paypal",
+            "sameAs": [
+              "https://www.facebook.com/technolockkeys_world/",
+              "https://twitter.com/techno_lock",
+              "https://api.whatsapp.com/send?phone=971504429045"
+            ]
+          }
+        },
+        { type: 'application/ld+json', json: {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "url": "https://www.tlkeys.com",
+            "logo": this.$settings.website.system_logo_black.l.url
+          }
+        }
+      ]
+    }
+  },
 }
 </script>
 
