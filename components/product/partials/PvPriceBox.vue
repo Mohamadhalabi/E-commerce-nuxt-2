d5d67ee97ba2d5bdbf966030d3ff7988ab1dd2aa
Mohamadhalabi <mhmd_Halabi99@hotmail.com>
2023-12-31 13:52:18 +0300

    Price box has been updated

--- components/product/partials/PvPriceBox.vue
+++ components/product/partials/PvPriceBox.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="price-box" :class="{ 'text-center': additionalProductItems }">
     <template v-if="product.discount && product.discount.length !=0">
-      <span
-        class="old-price mt-auto mb-auto"
-        :class="{ 'highlighted': hasDuplicate(product.discount.value), 'mt-1': gridPrice }"
-      >
-        {{product.price.value}}{{product.price.currency}}
-      </span>
+      {{product.discount.length}}
       <span
         v-if="product.discount.type =='fixed'"
         :class="{ 'highlighted': hasDuplicate(product.discount.value) }"
@@ -19,27 +14,33 @@
         class="product-price price-color font-weight-bold mt-0">
         {{(product.price.value - (product.price.value * (product.discount.value / 100))).toFixed(2)}}{{product.price.currency}}
       </span>
+      <span
+        class="old-price mt-auto mb-auto"
+        :class="{ 'highlighted': hasDuplicate(product.discount.value), 'mt-1': gridPrice }"
+      >
+            {{product.price.value}}{{product.price.currency}}
+      </span>
     </template>
 
     <template v-else-if="product.is_sale==1 && (product.price.value != product.sale_price.value)">
       <span
-        v-if="gridPrice"
-        class="old-price mt-1"
-        :class="{ 'highlighted': hasDuplicate(product.price.value) }"
-      >
-        {{ product.price.currency + product.price.value }}
+        :class="{ 'highlighted': hasDuplicate(product.sale_price.value) }"
+        class="product-price price-color font-weight-bold mt-0">
+        {{ product.sale_price.currency + product.sale_price.value }}
       </span>
       <span
-        v-else
+        v-if="!gridPrice"
         class="old-price mt-auto mb-auto"
         :class="{ 'highlighted': hasDuplicate(product.price.value) }"
       >
         {{ product.price.currency + product.price.value }}
       </span>
       <span
-        :class="{ 'highlighted': hasDuplicate(product.sale_price.value) }"
-        class="product-price price-color font-weight-bold mt-0">
-        {{ product.sale_price.currency + product.sale_price.value }}
+        v-else
+        class="old-price mt-1"
+        :class="{ 'highlighted': hasDuplicate(product.price.value) }"
+      >
+        {{ product.price.currency + product.price.value }}
       </span>
     </template>
     <template v-else>
