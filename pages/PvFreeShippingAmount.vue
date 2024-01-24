cfc08ee71a4aa54476593e4df8bbf229bd31f5ed
Mohamadhalabi <mhmd_Halabi99@hotmail.com>
2024-01-24 16:49:10 +0300

    * Generate Word and excel activated

--- pages/PvFreeShippingAmount.vue
+++ pages/PvFreeShippingAmount.vue
@@ -19,12 +19,12 @@
         <button type="button" class="border-0 bg-white" @click="generatePdf()">
           <img src="../static/images/pdf-logo.png" class="pdf-logo" alt="PDF Button">
         </button>
-<!--        <button type="button" class="border-0 bg-white" @click="generateExcel()">-->
-<!--          <img src="../static/images/excel.webp" class="excel-logo" alt="Excel Button">-->
-<!--        </button>-->
-<!--        <button type="button" class="border-0 bg-white" @click="generateWord()">-->
-<!--          <img src="../static/images/word-logo.webp" class="word-logo" alt="word Button">-->
-<!--        </button>-->
+        <button type="button" class="border-0 bg-white" @click="generateExcel()">
+          <img src="../static/images/excel.webp" class="excel-logo" alt="Excel Button">
+        </button>
+        <button type="button" class="border-0 bg-white" @click="generateWord()">
+          <img src="../static/images/word-logo.webp" class="word-logo" alt="word Button">
+        </button>
       </div>
     </div>
   </div>
