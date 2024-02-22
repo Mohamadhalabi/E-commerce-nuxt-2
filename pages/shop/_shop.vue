7c07e68a9abd8283b7c71dbf3c67ee48a2067b4b
Mohamadhalabi <mhmd_Halabi99@hotmail.com>
2024-02-22 16:28:36 +0300

    * Adding Faq to the manufacturer (faq schema to be added in the next commit)

--- pages/shop/_shop.vue
+++ pages/shop/_shop.vue
@@ -34,7 +34,86 @@
             @filters="displayFilters"
           />
           <div class="category-desc mt-3">
-            <div v-if="description" v-html="description"/>
+            <pv-tabs class="product-single-tabs">
+              <ul
+                class="nav nav-tabs"
+                role="tablist"
+                style="position: sticky;margin-bottom: 10px;">
+                <li
+                  class="nav-item"
+                  v-if="description && !isEmpty(description)"
+                >
+                  <a
+                    id="product-tab-desc"
+                    class="nav-link active"
+                    data-toggle="tab"
+                    href="#product-desc-content"
+                    role="tab"
+                    aria-controls="product-desc-content"
+                    aria-selected="true"
+                  >{{ $t("products.Description") }}</a
+                  >
+                </li>
+
+                <li class="nav-item" v-if="frequently_asked_questions && !isEmpty(frequently_asked_questions)">
+                  <a
+                    id="product-tab-faq"
+                    class="nav-link"
+                    data-toggle="tab"
+                    href="#faq-content"
+                    role="tab"
+                    aria-controls="faq-content"
+                    aria-selected="true"
+                  >{{ $t("products.FAQ") }} ({{frequently_asked_questions.length}})</a
+                  >
+                </li>
+              </ul>
+
+              <div class="tab-content">
+                <div
+                  v-if="description && !isEmpty(description)"
+                  id="product-desc-content"
+                  class="tab-pane fade show active"
+                  role="tabpanel"
+                  aria-labelledby="product-tab-desc"
+                  v-html="description"
+                  ref="contentContainer"
+                />
+                <div
+                  v-if="frequently_asked_questions && !isEmpty(frequently_asked_questions)"
+                  id="faq-content"
+                  class="tab-pane fade show active"
+                  role="tabpanel"
+                  aria-labelledby="faq-content"
+                  ref="contentContainer"
+                >
+                  <div v-for="(faq, index) in frequently_asked_questions" :key="index">
+                    <b-card no-body class="mb-1">
+                      <b-button
+                        class="pdf-button"
+                        block
+                        v-b-toggle="'accordion-' + index"
+                        @click="flipArrow(index)"
+                      >
+                        <div class="pdf-icon"></div>
+                        <h2 class="m-0 pdf-faq-title">{{faq[0]}}
+                          <span :class="['accordion-arrow', {'flipped': isArrowFlipped(index)}]" style="float: right">&#9660;</span>
+                        </h2>
+                      </b-button>
+                      <b-collapse :id="'accordion-' + index" accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                          <b-card-text>
+                            <p class="m-0" v-html="faq[1]"></p>
+                          </b-card-text>
+                        </b-card-body>
+                      </b-collapse>
+                    </b-card>
+                  </div>
+                </div>
+
+              </div>
+            </pv-tabs>
+
           </div>
         </div>
         <div class="sidebar-shop col-lg-2 order-lg-first mobile-sidebar p-0" sticky-container>
@@ -66,9 +145,16 @@
 import ShopBanner from '~/components/shop/PvShopBanner';
 import PvProductList from "~/components/shop/PvProductList.vue";
 import SidebarFilter from "~/components/shop/SidebarFilter.vue";
+import PvContactus from "~/components/product/tabs/PvContactus.vue";
+import PvProduct from "~/components/product/card/PvProduct.vue";
+import PvPdf from "~/components/product/tabs/PvPdf.vue";
+import PvReviews from "~/components/product/tabs/PvReviews.vue";
+import PvTabs from "~/components/features/PvTabs.vue";
+import {isEmpty} from "lodash";
 export default {
   name: 'ShopTemplate',
   components: {
+    PvTabs, PvReviews, PvPdf, PvProduct, PvContactus,
     PvProductList,
     ShopBanner,
     SidebarFilter,
@@ -101,6 +187,8 @@ export default {
       slug: null,
       title: null,
       openModal: false,
+      frequently_asked_questions: [],
+      flippedArrows: [],
     };
   },
   created() {
@@ -115,7 +203,7 @@ export default {
       this.description = this.manufacturer.description;
       this.slugtype = "manufacturer";
       this.title = this.manufacturer.title;
-
+      this.frequently_asked_questions = this.manufacturer.frequently_asked_questions
 
     } else if (this.brand) {
       this.slug = this.brand.slug;
@@ -126,6 +214,7 @@ export default {
 
   },
   methods: {
+    isEmpty,
     displayFilters(val){
       if(val){
         this.openModal = true
@@ -138,6 +227,17 @@ export default {
     changeDirection(directionBy) {
       this.directionBy = directionBy;
       this.getProduct(this.slug);
+    },
+    flipArrow(index) {
+      const currentIndex = this.flippedArrows.indexOf(index);
+      if (currentIndex === -1) {
+        this.flippedArrows.push(index);
+      } else {
+        this.flippedArrows.splice(currentIndex, 1);
+      }
+    },
+    isArrowFlipped(index) {
+      return this.flippedArrows.includes(index);
     }
   }
 }
