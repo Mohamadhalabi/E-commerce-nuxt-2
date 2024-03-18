e304ab4b32ef07df79906684c7540f72452a17b6
kay56 <info@tlkeys.com>
2024-03-18 12:08:15 +0300

    * modal image size has been changed

--- components/product/ImageModal.vue
+++ components/product/ImageModal.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <b-modal v-model="showModal" hide-footer centered hide-header>
-      <img :src="imageUrl" alt="Modal Image">
-      <div>
-      </div>
     </b-modal>
+    <img v-if="this.showModal" :src="imageUrl" alt="Modal Image" class="modal-image">
+
     <button
         v-if="this.showModal"
         title="Close (Esc)"
@@ -45,5 +44,21 @@ export default {
   right: 0;
   border: 0;
   z-index: 10000
+}
+.modal-image{
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+  z-index: 9999999999;
+  padding: 50px;
+}
+
+@media screen and (max-width: 992px){
+  .modal-image{
+    padding: 10px;
+  }
 }
 </style>
