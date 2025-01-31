9a61c08184330ebe3ccb212f0f583046e3aebcc3
Mohamad al halabi <info@tlkeys.com>
2025-01-31 16:12:23 +0300

    nuxt2 migrated to nuxt bridge to be continued

--- pages/index.vue
+++ pages/index.vue
@@ -1,133 +1,11 @@
 <template>
-  <main class="main">
-    <LazyHydrate when-idle>
-      <pv-stories class="desktop-only" />    
-    </LazyHydrate>
-    <LazyHydrate when-idle>
-      <pv-intro-section/>
-    </LazyHydrate>
-    <LazyHydrate when-idle>
-      <home-page-search />
-    </LazyHydrate>
-    <LazyHydrate when-idle>
-      <pv-slider-banner class="desktop-only"  />
-    </LazyHydrate>
-    <div
-        class="container-fluid">
-        <div class="container">
-          <LazyHydrate when-visible> 
-          <pv-latest-products
-            :isIndexPage ="true"
-            :collectionTitle="$t('home.latestProducts')"
-            :link="'new-arrival'"
-          />
-          </LazyHydrate>
-        </div>
-      </div>
-      <div
-        <div class="container">
-          <LazyHydrate when-visible>
-          <pv-best-selling
-            :isIndexPage ="true"
-            :collectionTitle="$t('home.bestSellingProducts')"
-            :link="'best-seller'"
-          />
-          </LazyHydrate>
-        </div>
-      <div
-        class="container-fluid">
-        <div class="container">
-          <LazyHydrate when-visible>
-          <pv-top-selling
-            :isIndexPage ="true"
-            :collectionTitle="$t('home.topSellingProduct')"
-            :link="'top-selling'"
-          />
-          </LazyHydrate>
-        </div>
-      </div>
-      <div class="container-fluid pt-4">
-        <div class="container large-screen-only">
-          <LazyHydrate when-visible>
-          <pv-line-banner class="mb-4" v-if="!isMobile" />
-          </LazyHydrate>
-        </div>
-      </div>
-      <div class="container">
-          <div class="product-widgets-container row pb-2">
-            <LazyHydrate when-visible>
-            <pv-on-sale-products
-              :collection-title="$t('home.onSaleProduct')"
-              :animation-delay="'200'"
-              :link="'discount'"
-            />
-            </LazyHydrate>
-            <LazyHydrate when-visible>
-            <pv-top-selling-three-products
-              :collection-title="$t('home.topSellingProduct')"
-              :animation-delay="'500'"
-              :link="'top-selling'"
-            />
-            </LazyHydrate>
-            <LazyHydrate when-visible>
-            <pv-new-arrival
-              :collection-title="$t('home.newarrivalproducts')"
-              :animation-delay="'800'"
-              :link="'new-arrival'"
-            />
-            </LazyHydrate>
-            <LazyHydrate when-visible>
-            <pv-free-shipping
-              :collection-title="$t('home.freeshoppingproducts')"
-              :animation-delay="'1100'"
-              :link="'free-shipping'"
-            />
-            </LazyHydrate>
-          </div>
-        </div>
-        <LazyHydrate when-visible>
-          <pv-manufacturers-section/>
-        </LazyHydrate>
-        <div style="background-color: #f07905">
-      <div class="container">
-        <div
-          class="p-3 justify-content-center">
-<!--          <b-form>-->
-<!--            <div class="row">-->
-<!--              <div class="col-lg-3 m-auto">-->
-<!--                <h6 class="mt-auto mb-auto mr-3 news-letter-h6">Subscribe to our newsletter</h6>-->
-<!--              </div>-->
-<!--              <div class="col-lg-6 m-auto">-->
-<!--                <b-form-group-->
-<!--                  id="input-group-1"-->
-<!--                  label="Enter your email address"-->
-<!--                  label-for="input-1"-->
-<!--                  description="We'll never share your email with anyone else."-->
-<!--                >-->
-<!--                  <b-form-input-->
-<!--                    id="input-1"-->
-<!--                    type="email"-->
-<!--                    class="m-0"-->
-<!--                    v-model="email"-->
-<!--                    placeholder="Enter email"-->
-<!--                    required-->
-<!--                  ></b-form-input>-->
-<!--                </b-form-group>-->
-<!--              </div>-->
-<!--              <div class="col-lg-3 m-auto">-->
-<!--                <button type="submit" @click="Subscribe()" class="subscribe">Subscribe</button>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </b-form>-->
-        </div>
-      </div>
-    </div>
-  </main>
+  <div>
+    <h1>Test</h1>
+  </div>
 </template>
 <script>
-import {mapGetters} from "vuex";
-import api from '~/api';
-import LazyHydrate from 'vue-lazy-hydration';
+// import {mapGetters} from "vuex";
+// import api from '~/api';
 export default {
   head() {
     const languagePrefix = this.$i18n.locale !== 'en' ? `/${this.$i18n.locale}` : '';
@@ -258,20 +136,20 @@ export default {
   },
   components: {
     // PvNewsletterModal,
-    LazyHydrate,
-    PvStories: () => import("~/components/home/PvStories"),
-    PvIntroSection: () => import("~/components/home/PvIntroSection.vue"),
-    HomePageSearch: () => import("~/components/home/HomePageSearch.vue"),
-    PvSliderBanner: () => import("~/components/home/PvSliderBanner.vue"),
-    PvLatestProducts: () => import("~/components/home/PvLatestProducts.vue"),
-    PvBestSelling: () => import("~/components/home/PvBestSelling.vue"),
-    PvTopSelling: () => import("~/components/home/PvTopSelling.vue"),
-    PvLineBanner: () => import("~/components/home/PvLineBanner.vue"),
-    PvOnSaleProducts: () => import("~/components/home/PvOnSaleProducts.vue"),
-    PvTopSellingThreeProducts: () => import("~/components/home/PvTopSellingThreeProducts.vue"),
-    PvNewArrival: () => import("~/components/home/PvNewArrival.vue"),
-    PvFreeShipping:() => import("~/components/home/PvFreeShipping.vue"),
-    PvManufacturersSection: () => import("~/components/home/PvManufacturersSection.vue"),
+    // LazyHydrate,
+    // PvStories: () => import("~/components/home/PvStories"),
+    // PvIntroSection: () => import("~/components/home/PvIntroSection.vue"),
+    // HomePageSearch: () => import("~/components/home/HomePageSearch.vue"),
+    // PvSliderBanner: () => import("~/components/home/PvSliderBanner.vue"),
+    // PvLatestProducts: () => import("~/components/home/PvLatestProducts.vue"),
+    // PvBestSelling: () => import("~/components/home/PvBestSelling.vue"),
+    // PvTopSelling: () => import("~/components/home/PvTopSelling.vue"),
+    // PvLineBanner: () => import("~/components/home/PvLineBanner.vue"),
+    // PvOnSaleProducts: () => import("~/components/home/PvOnSaleProducts.vue"),
+    // PvTopSellingThreeProducts: () => import("~/components/home/PvTopSellingThreeProducts.vue"),
+    // PvNewArrival: () => import("~/components/home/PvNewArrival.vue"),
+    // PvFreeShipping:() => import("~/components/home/PvFreeShipping.vue"),
+    // PvManufacturersSection: () => import("~/components/home/PvManufacturersSection.vue"),
   },
 
   data: function () {
@@ -281,7 +159,7 @@ export default {
     };
   },
   computed: {
-    ...mapGetters("rtlStore", ["getIsAr"]),
+    // ...mapGetters("rtlStore", ["getIsAr"]),
   },
   methods: {
     // async checkIfMobile() {
@@ -299,47 +177,47 @@ export default {
     //   }
     // },
 
-    handleResize() {
-      this.isMobile = window.innerWidth < 768;
-    },
-    Subscribe(){
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
-        // Assuming `api` is an instance of Axios or similar HTTP client
-        api.post("/subscribe", { email: this.email }).then((response) => {
-        }).catch((error) => {
-          if (error.response.status === 400) {
-            this.$notify({
-              group: "errorMessage",
-              type: "error",
-              text:  this.$t("alerts.emailAlready"),
-            });
-          } else {
-            console.error(error);
-          }
-        });
-      } else {
-        this.$notify({
-          group: "errorMessage",
-          type: "error",
-          text:  this.$t("alerts.enterValidEmail"),
-        });
-      }
-    }
+    // handleResize() {
+    //   this.isMobile = window.innerWidth < 768;
+    // },
+    // Subscribe(){
+    //   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)) {
+    //     // Assuming `api` is an instance of Axios or similar HTTP client
+    //     api.post("/subscribe", { email: this.email }).then((response) => {
+    //     }).catch((error) => {
+    //       if (error.response.status === 400) {
+    //         this.$notify({
+    //           group: "errorMessage",
+    //           type: "error",
+    //           text:  this.$t("alerts.emailAlready"),
+    //         });
+    //       } else {
+    //         console.error(error);
+    //       }
+    //     });
+    //   } else {
+    //     this.$notify({
+    //       group: "errorMessage",
+    //       type: "error",
+    //       text:  this.$t("alerts.enterValidEmail"),
+    //     });
+    //   }
+    // }
   },
   mounted() {
-    if(process.client) {
-      this.isMobile = window.innerWidth < 768;
-      window.addEventListener('resize', this.handleResize);
-    }
+    // if(process.client) {
+    //   this.isMobile = window.innerWidth < 768;
+    //   window.addEventListener('resize', this.handleResize);
+    // }
     // this.checkIfMobile();
     // window.addEventListener('resize', this.checkIfMobile);
 
   },
-  beforeDestroy() {
-    // Remove the resize event listener when the component is destroyed
-    if(process.client)
-    window.removeEventListener('resize', this.handleResize);
-  },
+  // beforeDestroy() {
+  //   // Remove the resize event listener when the component is destroyed
+  //   if(process.client)
+  //   window.removeEventListener('resize', this.handleResize);
+  // },
 }
 </script>
 <style>
