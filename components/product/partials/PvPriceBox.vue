fdc866f895e599d3ed946d3ed2432e6090aa9ae6
Mohamadhalabi <mhmd_halabi99@hotmail.com>
2025-07-15 13:03:24 +0300

    AED currency symbol has been changed

--- components/product/partials/PvPriceBox.vue
+++ components/product/partials/PvPriceBox.vue
@@ -1,60 +1,62 @@
 <template>
-  <div class="price-box">
-    <template v-if="product.discount && product.discount.length != 0">
+  <div class="price-box d-flex align-items-center">
+    <!-- Discount: Fixed -->
+    <template v-if="product.discount && product.discount.length !== 0">
       <span
-        v-if="product.discount.type == 'fixed'"
-        :class="[smallProducts ? 'small-price-color' : (productPage ? 'product-detail-price' : 'price-color'), 'font-weight-bold', { 'highlighted': hasDuplicate(product.discount.value) }]"
-      >
-        {{ (product.price.value - (product.discount.value * product.price.exchange_rate)).toFixed(2) }}{{ product.price.currency }}
-      </span>
+        v-if="product.discount.type === 'fixed'"
+        :class="[priceClass(product.discount.value)]"
+        v-html="formatPrice(
+          product.price.value - product.discount.value * product.price.exchange_rate,
+          product.price.currency
+        )"
+      ></span>
+
+      <!-- Discount: Percentage -->
       <span
         v-else
-        :class="[smallProducts ? 'small-price-color' : (productPage ? 'product-detail-price' : 'price-color'), 'font-weight-bold', { 'highlighted': hasDuplicate(product.discount.value) }]"
-      >
-        {{ (product.price.value - (product.price.value * (product.discount.value / 100))).toFixed(2) }}{{ product.price.currency }}
-      </span>
+        :class="[priceClass(product.discount.value)]"
+        v-html="formatPrice(
+          product.price.value - (product.price.value * product.discount.value) / 100,
+          product.price.currency
+        )"
+      ></span>
+
+      <!-- Old price -->
       <span
-        class="old-price mb-0"
-        :class="[{ 'highlighted': hasDuplicate(product.discount.value), 'mt-1': gridPrice }]"
-      >
-        {{ product.price.value }}{{ product.price.currency }}
-      </span>
+        class="old-price"
+        :class="{ highlighted: hasDuplicate(product.discount.value), '': gridPrice }"
+        v-html="formatOldPrice(product.price.value, product.price.currency)"
+      ></span>
     </template>
 
-    <template v-else-if="product.is_sale == 1 && (product.price.value != product.sale_price.value)">
+    <!-- Sale Price -->
+    <template v-else-if="product.is_sale === 1 && product.price.value !== product.sale_price.value">
       <span
-        :class="[smallProducts ? 'small-price-color' : (productPage ? 'product-detail-price' : 'price-color'), 'font-weight-bold', { 'highlighted': hasDuplicate(product.sale_price.value) }]"
-      >
-        {{ product.sale_price.currency + product.sale_price.value }}
-      </span>
+        :class="[priceClass(product.sale_price.value)]"
+        v-html="formatPrice(product.sale_price.value, product.sale_price.currency)"
+      ></span>
+
       <span
-        v-if="!gridPrice"
-        class="old-price mt-1 mb-0"
-        :class="{ 'highlighted': hasDuplicate(product.price.value) }"
-      >
-        {{ product.price.currency + product.price.value }}
-      </span>
-      <span
-        v-else
-        class="old-price mt-1"
-        :class="{ 'highlighted': hasDuplicate(product.price.value) }"
-      >
-        {{ product.price.currency + product.price.value }}
-      </span>
+        class="old-price"
+        :class="{ highlighted: hasDuplicate(product.price.value) }"
+        v-html="formatOldPrice(product.price.value, product.price.currency)"
+      ></span>
     </template>
 
+    <!-- Regular Price -->
     <template v-else>
       <span
-        :class="[smallProducts ? 'small-price-color' : (productPage ? 'product-detail-price' : 'price-color'), { 'highlighted': hasDuplicate(product.price.value), 'homePageSearch': homePageSearch === true }]"
-      >
-        {{ product.price.currency + product.price.value }}
-      </span>
+        :class="[
+          smallProducts ? 'small-price-color' : productPage ? 'product-detail-price' : 'price-color',
+          { highlighted: hasDuplicate(product.price.value), homePageSearch: homePageSearch === true }
+        ]"
+        v-html="formatPrice(product.price.value, product.price.currency)"
+      ></span>
     </template>
   </div>
 </template>
 
 <script>
-import { isEmpty } from "lodash";
 import { mapGetters } from "vuex";
 
 export default {
@@ -67,21 +69,54 @@ export default {
     smallProducts: Boolean,
     productPage: Boolean,
   },
-  data: function () {
-    return {
-      isEmpty,
-    };
+  computed: {
+    ...mapGetters("compare", ["getList"]),
   },
   methods: {
     hasDuplicate(status) {
       if (this.highlightCommon) {
-        const count = this.getList.filter(product => product.is_free_shipping === status).length;
+        const count = this.getList.filter((product) => product.is_free_shipping === status).length;
         return count >= 2;
       }
+      return false;
     },
-  },
-  computed: {
-    ...mapGetters("compare", ["getList"]),
+    priceClass(value) {
+      return [
+        this.smallProducts
+          ? "small-price-color"
+          : this.productPage
+          ? "product-detail-price"
+          : "price-color",
+        "font-weight-bold",
+        { highlighted: this.hasDuplicate(value) },
+      ];
+    },
+    currencySymbol(currency) {
+      if (currency === "AED") {
+        return `<img src="/images/currencies/uae_currency.webp" alt="AED" style="height: 32px; display: inline-block; vertical-align: middle; margin-right: 4px;" />`;
+      }
+      return `${currency}`;
+    },
+    currencySymbolSmall(currency) {
+      if (currency === "AED") {
+        return `<img src="/images/currencies/uae_currency_gray.webp" alt="AED" style="height: 22px; display: inline-block; vertical-align: middle; margin-right: 4px;" />`;
+      }
+      return `${currency}`;
+    },
+    formatPrice(value, currency) {
+      const formatted = Number(value).toFixed(2);
+      if (currency === "AED") {
+        return `${this.currencySymbol(currency)}${formatted}`;
+      }
+      return `${currency}${formatted}`;
+    },
+    formatOldPrice(value, currency) {
+      const formatted = Number(value).toFixed(2);
+      if (currency === "AED") {
+        return `${this.currencySymbolSmall(currency)}${formatted}`;
+      }
+      return `${currency}${formatted}`;
+    },
   },
 };
 </script>
@@ -92,5 +127,8 @@ export default {
 }
 .homePageSearch {
   font-size: 16px;
+}
+.price-box img {
+  vertical-align: middle;
 }
 </style>
