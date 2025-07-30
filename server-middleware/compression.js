fd90335f4992816bd2bef10daa2d17abdb108c54
Mohamadhalabi <mhmd_halabi99@hotmail.com>
2025-07-30 16:36:36 +0300

    nuxt compression added

--- server-middleware/compression.js
+++ server-middleware/compression.js
@@ -1,2 +1,7 @@
+// server-middleware/compression.js
 const compression = require('compression');
-module.exports = compression();
+
+module.exports = compression({
+  threshold: 0, // apply compression for all sizes
+  level: 9      // maximum compression
+});
